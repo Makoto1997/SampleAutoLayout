e7b07dbd3580c0a2db3b127768a671e8220e1813
Makoto <>
2021-05-28 23:52:26 +0900

    initial import

---
+++ SampleAutoLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleAutoLayout
+//
+//  Created by Makoto on 2021/05/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
